81b8dc1d58784d84105c8ab4e45ea5e2a5e4ef21
shlokkvaishnav <shlok9640@gmail.com>
2025-12-18 11:24:18 +0530

    feat: initial working build of vector engine (HNSW + MMap)
    - Implemented HNSW graph structure. - Added MMap storage. - Performance: ~4.9k TPS insert, ~0.14ms search.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/shlok/OneDrive/Desktop/Projects/nano-db
+# Build directory: C:/Users/shlok/OneDrive/Desktop/Projects/nano-db/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/shlok/OneDrive/Desktop/Projects/nano-db/tests
+# Build directory: C:/Users/shlok/OneDrive/Desktop/Projects/nano-db/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
